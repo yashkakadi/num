9d5aec61cfd4b4299858e1d5b40860df63a1736a
yashkakadi <143870757+yashkakadi@users.noreply.github.com>
2023-10-31 16:45:01 +0530

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,6 @@
+// generation of random value
+var y = Math.floor(Math.random() * 10+1);
+// count of attempts
+// until hit
+  
+// function for the number sent by the user
---
+++ login.js
@@ -0,0 +1,8 @@
+// Create the login function here.
+function login()
+{
+    player_name = document.getElementById("playername").value;
+    localStorage.setItem("player_name", player_name);
+
+    window.location = "gamepage.html";
+}
